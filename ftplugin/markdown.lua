3aef8090a1f90574d55dff5cdea581a566a72020
mvickers <vickers.mike@gmail.com>
2022-09-20 22:14:34 -0400

    updated sqid keybindings

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,8 +2,9 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
 
-keymap('x', '<leader>i', ':lua require"sqid".web()<CR>', opts)
-keymap('x', '<leader><leader>i', ':lua require"sqid".web_to_file()<CR>', opts)
+keymap('x', '<leader>si', ':lua require"sqid".web()<CR>', opts)
+keymap('x', '<leader>sf', ':lua require"sqid".web_to_file()<CR>', opts)
+keymap('x', '<leader>sw', ':lua require"sqid".pop_window()<CR>', opts)
 vim.cmd[[:setlocal wrap linebreak nolist]]
 vim.cmd[[:let g:vim_markdown_frontmatter = 1]]
 vim.cmd[[:setlocal spell spelllang=en]]
